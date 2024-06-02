a8686db24bda7a9265453d784d6d082bcc7c1e41
Aniruddha Rawool <aniruddha.212074101@vcet.edu.in>
2024-06-02 12:06:34 +0530

    create user done

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -1,9 +1,27 @@
 import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
 
 const createUser = async (req, res) => {
     try {
-        User.create
+        const { name, email, password,location} = req.body;
+        if(!name || !email || !password || !location){
+            return res.status(400).json({message: "All input is required"});
+        }
+
+        const userExists = await User.findOne({ email});
+        if(userExists){
+            return res.status(400).json({message: "User already exists"});
+        }
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const user = new User({ name, email, password: hashedPassword,location });
+        await user.save();
+        return res.status(201).json({ data: user });
     } catch (error) {
+        console.log("Error in creating user: ", error.message);
         return res.status(500).json({ error: error.message });
     }
 }
+
+export { createUser };
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,8 +1,9 @@
 import express from "express";
+import { createUser } from "../controller/user.controller.js";
 
 const router = express.Router();    
 
-router.post('/creatuser')
+router.post('/creatuser',createUser)
 
 
 export default router;
