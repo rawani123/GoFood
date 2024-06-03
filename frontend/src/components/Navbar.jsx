1882e5f4d397a765e582e62d8fff80a195eea071
Aniruddha Rawool <aniruddha.212074101@vcet.edu.in>
2024-06-03 10:51:09 +0530

    navbar done

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+
+  const navigate = useNavigate();
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-success">
@@ -20,17 +23,64 @@ const Navbar = () => {
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+
+          <div
+            className="collapse navbar-collapse justify-content-between"
+            id="navbarNavAltMarkup"
+          >
             <div className="navbar-nav">
-              <Link className="nav-link active" aria-current="page" to="#">
+              <Link
+                className="nav-link active fs-4 active mx-4"
+                aria-current="page"
+                to="/"
+              >
                 Home
               </Link>
-              <Link className="nav-link" to="/login">
-                Login
-              </Link>
-              <Link className="nav-link" to="/signup">
-                Signup
-              </Link>
+              {localStorage.getItem("token") ? (
+                <Link
+                  className="nav-link active fs-4 active mx-4"
+                  aria-current="page"
+                  to="/"
+                >
+                  My Orders
+                </Link>
+              ) : (
+                ""
+              )}
+            </div>
+            <div className="d-flex gap-2">
+              {localStorage.getItem("token") ? (<>
+                <Link
+                    className="btn bg-white text-success mx-1 px-3"
+                    to="/login"
+                  >
+                    My Cart 
+                  </Link>
+                <div onClick={() => {localStorage.clear()
+                  navigate('/')
+                  window.location.reload()
+                }}
+                className="btn bg-danger text-white mx-1 px-3"
+              >
+                logout
+              </div>  
+              </>
+              ) : (
+                <>
+                  <Link
+                    className="btn bg-white text-success mx-1 px-3"
+                    to="/login"
+                  >
+                    Login
+                  </Link>
+                  <Link
+                    className="btn bg-white text-success mx-1 px-3"
+                    to="/signup"
+                  >
+                    Signup
+                  </Link>
+                </>
+              )}
             </div>
           </div>
         </div>
